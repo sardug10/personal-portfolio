872cb748a919acb6e20bf0a1fcaeb26a3e745efb
Sarthak Duggal <52778617+sardug10@users.noreply.github.com>
2021-01-31 22:58:30 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// const projectsSection = document.querySelector('.projects')
+// const icons = Array.from(document.querySelectorAll('.header__icons'));
+// const mediaLinks = document.querySelector('.header__links');
+
+// const initialCoords = projectsSection.getBoundingClientRect()
+// // console.log(initialCoords)
+
+// window.addEventListener('scroll',()=>{
+//     // console.log(window.scrollY)
+//     if(window.scrollY >= 1400 && window.scrollY < 2000){
+//         icons.forEach(icon=>{
+//             icon.classList.add('color-white')
+//         })
+//     } else if(window.scrollY < 1500){
+//         icons.forEach(icon=>{
+//             icon.classList.remove('color-white')
+//         })
+//     } else{
+//         icons.forEach(icon=>{
+//             icon.classList.remove('color-white')
+//         })
+//     }
+// })
