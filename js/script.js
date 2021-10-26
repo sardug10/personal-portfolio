91f7f190320470a83dfc02837618c1643af61965
Sarthak Duggal <52778617+sardug10@users.noreply.github.com>
2021-10-27 03:36:14 +0530

    27-10-2021

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
-// const projectsSection = document.querySelector('.projects')
-// const icons = Array.from(document.querySelectorAll('.header__icons'));
-// const mediaLinks = document.querySelector('.header__links');
-
-// const initialCoords = projectsSection.getBoundingClientRect()
-// // console.log(initialCoords)
-
-// window.addEventListener('scroll',()=>{
-//     // console.log(window.scrollY)
-//     if(window.scrollY >= 1400 && window.scrollY < 2000){
-//         icons.forEach(icon=>{
-//             icon.classList.add('color-white')
-//         })
-//     } else if(window.scrollY < 1500){
-//         icons.forEach(icon=>{
-//             icon.classList.remove('color-white')
-//         })
-//     } else{
-//         icons.forEach(icon=>{
-//             icon.classList.remove('color-white')
-//         })
-//     }
-// })
+VanillaTilt.init(document.querySelectorAll(".projects__card"), {
+    max: 15,
+    speed: 400,
+    perspective:2000
+});
